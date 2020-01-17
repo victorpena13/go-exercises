b9a07a31621606bfe9d4ab39109cc216845fe51d
victorpena13 <victor.pena1392@gmail.com>
2020-01-17 11:55:36 -0600

    Create inferringType.go

---
+++ inferringType.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var daysOnVacation = 9 
+
+	//Define hoursInDay using var and = below:
+
+	var hoursInDay = 24
+
+	fmt.Println("You have spent", daysOnVacation * hoursInDay, "hours spent on vacation")
+}
