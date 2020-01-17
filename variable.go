a1205fdcf298366ef3f5c0c7b100172daadf8b91
victorpena13 <victor.pena1392@gmail.com>
2020-01-16 21:08:19 -0600

    Create variable.go
    a variable can be manipulated unlike a constant

---
+++ variable.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numOfFlavors int
+	numOfFlavors = 57
+
+	  // Assign a value for numOfFlavors below:
+	var flavorScale float32 = 5.8 
+
+	fmt.Println(numOfFlavors)
+	fmt.Println(flavorScale)
+}
