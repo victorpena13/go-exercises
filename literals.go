8a9bf4ac4b952a774562e6aa0c643b861dc92e13
victorpena13 <victor.pena1392@gmail.com>
2020-01-16 19:23:35 -0600

    Create literals.go
    literals are values that are not contained inside a variable.
    they literally what they say they are

---
+++ literals.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Add a fmt.Println() statement
+  // that prints 2235 * 1231
+  fmt.Println(2235 * 1231)
+}
+
+//literals are values that are not contained inside a variable. 
+//they literally what they say they are
