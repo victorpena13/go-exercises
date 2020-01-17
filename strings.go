6375b5877979efa9324c3756ea06f1c5c5073b76
victorpena13 <victor.pena1392@gmail.com>
2020-01-16 21:12:16 -0600

    Create strings.go

---
+++ strings.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Define a string variable called favoriteSnack
+
+	var favoriteSnack string
+
+	//Assign a value to favoriteSnack
+
+	favoriteSnack = "almond joy"
+
+  // Print out the message
+  // "My favorite snack is "
+  // followed by the value in
+  // favoriteSnack
+
+  fmt.Println("My favorite snack is " + favoriteSnack)
+}
