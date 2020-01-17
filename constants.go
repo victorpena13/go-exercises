392e619ab743badbf69626b019689b0e96ec06a7
victorpena13 <victor.pena1392@gmail.com>
2020-01-16 19:27:41 -0600

    Create constants.go
    //const are values that can be named
    //but their value cannot be manipulated

---
+++ constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  // Create the constant earthsGravity
+  // here and set it to 9.80665
+  
+  // Here's where we print out the gravity:
+  const earthsGravity = 9.80665
+  fmt.Println(earthsGravity)
+}
+
+//const are values that can be named 
+//but their value cannot be manipulated
