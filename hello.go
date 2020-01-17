58d56a4ac074326074c52e5603b9f554a4069f80
victorpena13 <victor.pena1392@gmail.com>
2020-01-16 19:14:33 -0600

    Update hello.go
    added comments to explain my first Hello World

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
+//a file that has package main will automatically 
+//execute the main function
 
 import "fmt"
+//fmt is package that is imported 
 
 func main() {
 	fmt.Println("Hello World")
 }
+//inside the function we are calling the imported 
+//fmt package to use the Println method to say "Hello World"
