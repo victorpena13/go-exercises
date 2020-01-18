96e64e9a2b863536ec031905e28320c1aa74a298
victorpena13 <victor.pena1392@gmail.com>
2020-01-17 18:19:14 -0600

    Create updatingVariables.go

---
+++ updatingVariables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	coolSneakers := 65.99
+	niceNecklace := 45.50
+
+	//define taxCalculation
+
+	var taxCalculation float64
+
+	taxCalculation += coolSneakers
+	taxCalculation += niceNecklace
+	taxCalculation *= .08875
+}
