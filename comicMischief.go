838f19e0c5870efb7176b02cb0e8d50c59eb0f01
victorpena13 <victor.pena1392@gmail.com>
2020-01-19 11:27:52 -0600

    Update comicMischief.go

--- comicMischief.go
+++ comicMischief.go
@@ -3,10 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var publisher string
-	var writer string
-	var artist string
-	var title string
-
-	  fmt.Println(title, "written by", writer, "drawn by", artist, "published by", publisher, "year:", year)
+  var publisher string
+  var writer string
+  var artist string
+  var title string
+  var year int
+  var pageNumber int
+  var grade float32
+  var condition float32
+  
+    title = "Mr.GoToSleep"
+  writer = "Tracey Hatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 1997
+  pageNumber = 14
+  condition = 6.5
+  
+  fmt.Println(title, "written by", writer, "drawn by", artist, "year:", year, "grade:", grade, "page number:", pageNumber, "publisher", publisher, "condition:",condition)
+  
 }
