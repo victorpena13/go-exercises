7c3cbd74f3a25d09f25e731288c2aa1391b0a2fc
victorpena13 <victor.pena1392@gmail.com>
2020-01-19 14:29:55 -0600

    Update printMethod.go
    updated comments to include details

--- printMethod.go
+++ printMethod.go
@@ -6,7 +6,20 @@ func main() {
   fmt.Println("Let's first see how", "the Println() method works.")
   fmt.Println("Notice that each statement adds a newline for us.")
   fmt.Println("There's also a default space", "between the string arguments.")
-
+  // result
+  // Let's first see how the Println() method works.
+  // Notice that each statement adds a newline for us.
+  // There's also a default space between the 
  
   fmt.Print("Print", "is", "different")
+  fmt.Print("see")
+  //result
+  //Printisdifferentsee
+
+
+  //summary:
+  //Println will format the statements with spaces and linebreaks
+  //print does not format the output to include spaces or linebreaks 
+
+
 }
