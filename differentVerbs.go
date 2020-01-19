a7a9d9b028029803bb9cc3fd3918247c1fdbe0e1
victorpena13 <victor.pena1392@gmail.com>
2020-01-19 14:57:42 -0600

    Create differentVerbs.go
    verbs and their data types

---
+++ differentVerbs.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+specialNum := 42
+fmt.Printf("This value's type is %T.", specialNum)
+//result: This value's type is int
+
+quote := "To do or not to do"
+fmt.Printf("This value's type is %T.", quote)
+//Result: This value's type is string
+}
