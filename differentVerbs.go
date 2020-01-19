0662551f2921d4e98a649bf17629d71da19424e5
victorpena13 <victor.pena1392@gmail.com>
2020-01-19 14:59:00 -0600

    Update differentVerbs.go

--- differentVerbs.go
+++ differentVerbs.go
@@ -10,4 +10,8 @@ fmt.Printf("This value's type is %T.", specialNum)
 quote := "To do or not to do"
 fmt.Printf("This value's type is %T.", quote)
 //Result: This value's type is string
+
+votingAge := 21
+fmt.Printf("You must be %d years old to vote.", votingAge)
+//Result: You must be 21 years old to vote.
 }
