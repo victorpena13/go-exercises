16cd5b4e5d995d297b66fc61e7351e0416a00996
victorpena13 <victor.pena1392@gmail.com>
2020-01-19 14:36:20 -0600

    Create Printf.go
    Printf allows you to use placeholders for variables in the string.

---
+++ Printf.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  animal1 := "cat"
+  animal2 := "dog"
+  
+  // Add your code below:
+  fmt.Printf("Are you a %v or a %v person?", animal1, animal2)
+  //result:
+  //Are you a cat or a dog person?
+  
+  //%v is a placeholder and is known as a
+  //verb in Go.
+}
