fe65e8afe9c9f172e3da236e0202fea7dd3f413b
victorpena13 <victor.pena1392@gmail.com>
2020-01-22 13:24:09 -0600

    Create functions.go
    created my first function in Go

---
+++ functions.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func summonNicole() {
+	fmt.Println("Hey Nicole, get over here!")
+}
+
+func main() {
+	//calling the function for the first time
+	summonNicole()
+
+	//calling the function for the second time
+	summonNicole()
+
+}
