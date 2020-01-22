671ebde77707d7e8e12c7e08b6170eb9a64c9eca
victorpena13 <victor.pena1392@gmail.com>
2020-01-21 19:11:26 -0600

    Update bankHeist.go
    created a condition that checks to see if a number is greater then 50. then checks to see if the number is over 50 and 70.

--- bankHeist.go
+++ bankHeist.go
@@ -16,7 +16,18 @@ func main() {
 	fmt.Println(eludedGuards)
 
 	if (eludedGuards >= 50) {
-		fmt.Println("Looks like You've managed to make it past the guards.  Good Job, but Remember,
-			this is the first step.")
+		fmt.Println("Looks like You've managed to make it past the guards.  Good Job, but Remember, this is the first step.")
+	} else {
+		isHeistOn := false
+		fmt.Println("Plan a better disguise")
+		fmt.Println(isHeistOn)
 	}
+
+	openedVault := rand.Intn(100)
+
+	if (isHeistOn == true && openedVault >= 70) {
+		fmt.Println("Grand and GO!")
+	}
+
+	fmt.Println("isHeistOn is currently:", isHeistOn)
 }
