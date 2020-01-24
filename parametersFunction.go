b3c27da89ea917f7bc5ee48ebcdf5a02ca46fe7c
victorpena13 <victor.pena1392@gmail.com>
2020-01-23 21:29:52 -0600

    Create parametersFunction.go
    added earthYears as a parameter to the computeMarsYears function

---
+++ parametersFunction.go
@@ -0,0 +1,21 @@
+package main
+import "fmt"
+
+// Update marsYear so that it takes earthYears
+// As a parameter
+func computeMarsYears(earthYears int) int {
+  // Remove earthYears definition within marsYear
+  earthYears := 30
+  
+  earthDays := earthYears * 365
+  marsYears := earthDays / 687
+  return marsYears
+}
+
+func main() {
+  myAge := 25
+  
+  // Call `marsYear` with `myAge`
+  myMartianAge := computeMarsYears()
+  fmt.Println(myMartianAge)
+}
