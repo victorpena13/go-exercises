fdda4bd00611948dc98b8a43c835ecf3dfe55647
victorpena13 <victor.pena1392@gmail.com>
2020-01-24 10:51:58 -0600

    Create reusingFunctions.go
    Write a function called specialComputation() which takes a parameter x, a float64, and returns a float64.

---
+++ reusingFunctions.go
@@ -0,0 +1,26 @@
+package main
+import (
+	"math"
+  "fmt"
+)
+
+// Define specialComputation() here
+func specialComputation(x float64) float64 {
+	
+}
+
+func main() {
+  var a, b, c, d float64
+  a = .0214
+  b = 1.02
+  c = 0.312
+  d = 4.001
+  
+  // Replace the following four lines with specialComputation()
+  a = math.Log2(math.Sqrt(math.Tanh(a)))
+  b = math.Log( math.Sqrt(math.Tanh(b)))
+  c = math.Log(math.Sqrt( math.Tan(c) ))
+  d = math.Log2(math.Sqrt(math.Tanh(d)))
+  
+  fmt.Println(a, b, c, d)
+}
