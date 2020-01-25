9fcc0351b87b0447f98b5bf88dd31c9274ed820e
victorpena13 <victor.pena1392@gmail.com>
2020-01-25 11:18:44 -0600

    Create goFunctions.go
    various functions of exercises.

---
+++ goFunctions.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+
+
+// Create the function calculateFuel() here
+
+
+// Create the function greetPlanet() here
+
+
+// Create the function cantFly() here
+
+
+// Create the function flyToPlanet() here
+
+
+func main() {
+  // Test your functions!
+  fuelGauge(2)
+  
+  // Create `planetChoice` and `fuel`
+  
+  // And then liftoff!
+  
+}
