d67d101e292dfd26135ba52d784a102e5e352f93
victorpena13 <victor.pena1392@gmail.com>
2020-01-25 11:54:48 -0600

    Update goFunctions.go
    Create a switch or a series of if & else statements to assign fuel in the following cases:
    
    Venus: 300000
    Mercury: 500000
    Mars: 700000
    If it’s not listed above: 0

--- goFunctions.go
+++ goFunctions.go
@@ -13,6 +13,16 @@ func fuelGauge(fuel int) {
 func calculateFuel(planet string) int {
   //this wil be the amount of fuel needed to fly to that planet
   var fuel int
+  switch planet {
+  case "Venus":
+  	fuel = 300000
+  case "Mercury":
+  	fuel = 500000
+  case "Mars":
+  	fuel = 700000
+  default:
+  	fuel = 0
+  }
 }
 
 // Create the function greetPlanet() here
